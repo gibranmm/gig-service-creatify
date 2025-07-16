74058753486544236e14c7ee13df80c84d7a443e
Gibran <gibranmaulanam@gmail.com>
2025-07-16 19:35:17 +0700

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser');
 require("dotenv").config();
 
 const app = express();
@@ -9,7 +10,7 @@ const gigRoutes = require("./routes/gigRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.use("/api/gigs", gigRoutes);
 app.use("/api/categories", categoryRoutes);
