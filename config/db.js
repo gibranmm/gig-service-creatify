050c4de854fe09796280434fef02f126c91b65bc
Gibran <gibranmaulanam@gmail.com>
2025-07-17 18:54:06 +0700

    fixing config

--- config/db.js
+++ config/db.js
@@ -1,19 +1,15 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+// Buat pool koneksi, bukan koneksi tunggal
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10, // jumlah maksimal koneksi
+  queueLimit: 0,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err.message);
-    process.exit(1);
-  }
-  console.log("Connected to MySQL database");
-});
-
-module.exports = db;
+module.exports = pool;
